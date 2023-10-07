034e95d6c639283e718bbec63f26e7542f19565b
Mohan-k7 <mohankilli27@gmail.com>
2023-10-07 19:37:48 +0530

    first commit

--- server/database/conn.js
+++ server/database/conn.js
@@ -9,8 +9,8 @@ async function connect(){
     const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true)
+    // const db = await mongoose.connect(getUri);
     const db = await mongoose.connect("mongodb+srv://mohan:mohan@cluster0.yvc1agc.mongodb.net/xxx?retryWrites=true&w=majority");
-   // const db = await mongoose.connect(ENV.ATLAS_URI);
     console.log("Database Connected")
     return db;
 }
